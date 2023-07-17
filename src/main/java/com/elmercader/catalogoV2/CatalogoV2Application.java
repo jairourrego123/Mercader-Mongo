3571cac77396c08b0db512b832bf13c2c3a542b9
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-07-17 17:21:34 -0500

    main class

---
+++ src/main/java/com/elmercader/catalogoV2/CatalogoV2Application.java
@@ -0,0 +1,13 @@
+package com.elmercader.catalogoV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoV2Application.class, args);
+	}
+
+}
