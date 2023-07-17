713c86284af70108f23733662ac0a2e87a54c00d
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-07-17 17:23:11 -0500

    Project data repositories using a Mongo interface

---
+++ src/main/java/com/elmercader/catalogoV2/repositories/GadgetRepository.java
@@ -0,0 +1,30 @@
+package com.elmercader.catalogoV2.repositories;
+
+import com.elmercader.catalogoV2.models.Gadget;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GadgetRepository extends MongoRepository<Gadget,Integer> {
+    @Query("{id: ?0}")
+    Optional<Gadget> getGadgetById(Integer id);
+
+    @Query("{category: ?0}")
+    List<Gadget> getGadgetByCategory(String category);
+
+    @Query("{name: {$regex:/?0/}}")
+    List<Gadget> getGadgetByName(String name);
+
+    @Query("{price:{$gte:?0, $lte: ?1}}")
+    List<Gadget> getGadgetByPrice(Double minPrice, Double maxPrice);
+    @Query("{availability: ?0}")
+    List<Gadget> getGadgetByAvailability(Boolean availability);
+
+
+
+
+
+
+}
