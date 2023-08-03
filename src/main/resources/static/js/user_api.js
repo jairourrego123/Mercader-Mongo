d534c1ec185c8bbefac46c1702ee47c2b490aa5c
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-08-03 18:07:32 -0500

    Update user and Open modal

--- src/main/resources/static/js/user_api.js
+++ src/main/resources/static/js/user_api.js
@@ -1,9 +1,15 @@
-const URL_BASE="http://localhost:8080";
+const URL_BASE="http://localhost:8080/api/user";
 let user = null
+$(document).ready(function(){
+
+    getAllUsers();
+
+
+    })
 
 function getUserByEmail(email) {
     $.ajax({
-        url: URL_BASE+"/api/user/emailexist/"+email,
+        url: URL_BASE+"/emailexist/"+email,
         type:"GET",
         datatype:"JSON"
     })
@@ -16,6 +22,167 @@ function getUserByEmail(email) {
         return true;
     })
 };
+
+function validateLogin() {
+    let email = document.getElementById("txtEmail").value;
+    let password = document.getElementById("txtPassword").value;
+    console.log(email)
+    console.log(password)
+
+    if (emailValidation(email)) {
+
+         $.ajax({
+                url: URL_BASE + "/"+email+"/"+password,
+                type:"GET",
+                datatype:"JSON"
+            })
+            .done(function(response){
+                console.log(response)
+                if(response){
+                    alert("Usuario con ingreso correcto")
+                    location.href ='/usuarios.html';}
+                 else
+                    alert("Usuario con ingreso incorrecto")
+                //TODO redirect to profile
+            })
+            .fail(function(jqXHR,textStatus,errorThrown){
+                console.log("Error en validateLogin. "+textStatus)
+                 alert("No se ha podido Logeear, Verifique sus credenciales")
+                 //TODO redirect
+            })
+        }
+
+     else{
+          alert("El formato Email no es valido")
+     }
+    
+};
+
+function updateTableUser(user){
+     $("#tableUser").find("tr:gt(0)").remove(); // modifica despues de la primera fila
+     let data = ""
+    for(let i=0;i < user.length; i++){
+
+        data += "<tr>"
+        data += "<td>"+user[i].name+"</td>"
+        data += "<td>"+user[i].identification + "</td>"
+        data += "<td>"+user[i].address + "</td>"
+        data += "<td>"+user[i].cellPhone + "</td>"
+        data += "<td>"+user[i].email + "</td>"
+        data += "<td>"+user[i].zone + "</td>"
+        data += "<td>"+user[i].type + "</td>"
+        data += "<td onclick=\"openModalUser("+user[i].id+")\">Actualizar</td>"
+        data += "<td onclick=\"deleteUser("+user[i].id+")\">Eliminar</td>"
+        data += "</tr>"
+    }
+     $("#tableUser > tbody:last-child").append(data);
+    console.log(data)
+
+
+}
+
+function openModalUser(idUser){
+
+
+    getUserId(idUser)
+    $("#txtId").val(user.id)
+    $("#txtIdentification").val(user.identification);
+    $("#txtName").val(user.name);
+    $("#txtAddress").val(user.address);
+    $("#txtCellPhone").val(user.cellPhone);
+    $("#txtEmail").val(user.email);
+    $("#txtEmail").prop("disabled",true)
+    $("#txtZone").val(user.zone);
+    $("#cbxType").val(user.type);
+    $("#modalUser").show();
+
+}
+function UpdateUser(idUser){
+
+      if (!passwordValidation($("#txtPassword").val())){
+        alert("La longitud de la contraseña debe ser mayor a 6 digitos")
+        return false
+        }
+      if (!(newPasswordValidation($("#txtPassword").val(),$("#txtConfirmedPassword").val()))){
+            alert("Las contraseñas no son iguales")
+        return false}
+
+      user = {
+            id:user.id,
+            identification:$("#txtIdentification").val(),
+            name:$("#txtName").val(),
+            address:$("#txtAddress").val(),
+            cellPhone:$("#txtCellPhone").val(),
+            email:$("#txtEmail").val(),
+            zone: $("#txtZone").val(),
+            type: $("#cbxType").val(),
+            password : $("#txtPassword").val()
+
+        }
+
+    body = JSON.stringify(user)
+    console.log(body);
+    $.ajax({
+
+        url: URL_BASE + "/update",
+        type: "PUT",
+        datatype: "JSON",
+        data:body,
+        contentType: "application/json;charset=UTF-8"
+    })
+    .done(function(response){
+        console.log(response);
+        getAllUsers();
+        alert("Usuario Modificado");
+        $("#modalUser").hide();
+
+    })
+    .fail(function(jqXHR,textStatus,errorThrown){
+        console.log("Error in UpdateUser." + textStatus);
+        alert("No se ha podido actualizar la informacion del Usuario");
+
+    })
+}
+function getUserId(idUser){
+    $.ajax({
+
+        url:URL_BASE + "/"+ idUser,
+        type: "GET",
+        datatype : "JSON",
+        async: false
+
+    })
+
+    .done(function(response){
+
+        console.log(response);
+        user = response;
+    })
+    .fail(function(jqXHR,textStatus,errorThrown){
+        console.log("Error in getUserId ")
+        alert("Hemos obtenido una falla obteniendo la informacion del Usuario. Por favor intente de nuevo mas tarde.")
+    })
+
+}
+function getAllUsers(){
+    $.ajax({
+
+        url:URL_BASE + "/all",
+        type: "GET",
+        datatype: "JSON"
+
+    })
+    .done(function(response){
+        console.log(response);
+        updateTableUser(response)
+    })
+    .fail(function(jqXHR,textStatus,errorThrown){
+        console.log("Error in getAllUsers. "+ textStatus );
+        alert("No se han podido cargar los Usuarios.");
+    })
+
+}
+
 function createUser() {
     let id = document.getElementById("txtId").value;
     let identification = document.getElementById("txtIdentification").value;
@@ -38,7 +205,7 @@ function createUser() {
                         identification:identification,
                         name:name,
                         address:address,
-                        cellphone:cellphone,
+                        cellPhone:cellphone,
                         email:email,
                         zone:zone,
                         type:type,
@@ -48,7 +215,7 @@ function createUser() {
                 let body = JSON.stringify(user);
                 console.log(body)
                 $.ajax({
-                            url:URL_BASE+"/api/user/new",
+                            url:URL_BASE+"/new",
                             type:"POST",
                             datatype:"JSON",
                             data:body,
@@ -56,8 +223,9 @@ function createUser() {
                         })
                         .done(function(response){
                                console.log(response)
-                               if(response)
+                               if(response){
                                     alert("Cuenta creada de forma correcta")
+                                    location.href ='/login.html';}
                                else
                                      alert("No fue posible crear la cuenta ")
                         })
@@ -85,37 +253,20 @@ function createUser() {
     // TODO redirect to home
 };
 
+function deleteUser(idUser){
 
-function validateLogin() {
-    let email = document.getElementById("txtEmail").value;
-    let password = document.getElementById("txtPassword").value;
-    console.log(email)
-    console.log(password)
-
-    if (emailValidation(email)) {
-
-         $.ajax({
-                url: URL_BASE + "/api/user/"+email+"/"+password,
-                type:"GET",
-                datatype:"JSON"
-            })
-            .done(function(response){
-                console.log(response)
-                if(response)
-                    alert("Usuario con ingreso correcto")
-                 else
-                    alert("Usuario con ingreso incorrecto")
-                //TODO redirect to profile
-            })
-            .fail(function(jqXHR,textStatus,errorThrown){
-                console.log("Error en validateLogin. "+textStatus)
-                 alert("No se ha podido Logeear, Verifique sus credenciales")
-                 //TODO redirect
-            })
-        }
-
-     else{
-          alert("El formato Email no es valido")
-     }
-    
-};
+        $.ajax({
+            url:URL_BASE + "/"+idUser,
+            type : "DELETE",
+            datatype : "JSON"
+        })
+        .done(function(response){
+           console.log(response)
+           getAllUsers()
+           alert("Usuario eliminado correctamente")
+        })
+        .fail(function(jqXHR,textStatus,errorThrown){
+            console.log("Error in deleteUser. " + textStatus);
+            alert("No ha sido posible eliminar al usuario.Por favor intentelo mas tarde.")
+        })
+}
