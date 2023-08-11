c6e902fada7e1072eff386c1f1772d154ff430e7
jairourrego123 <57160592+jairourrego123@users.noreply.github.com>
2023-08-11 17:13:46 -0500

    Repository Order

--- Backend/src/main/java/com/elmercader/catalogoV2/repositories/OrderRepository.java
+++ Backend/src/main/java/com/elmercader/catalogoV2/repositories/OrderRepository.java
@@ -1,2 +1,15 @@
-package com.elmercader.catalogoV2.repositories;public interface OrderRepository {
+package com.elmercader.catalogoV2.repositories;
+
+import com.elmercader.catalogoV2.models.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends MongoRepository<Order,Integer> {
+    @Query("{status: ?0}")
+    public List<Order> findOrderByStatus(String status);
+    @Query("{salesman.zone: ?0}")
+    public List<Order> findOrderByZone(String zone);
 }
